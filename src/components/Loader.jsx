b964e9c4c8c1f4563dba6b79d7e064d66392dee4
abdullah <abdullah.pluton@gmail.com>
2024-09-13 20:31:40 +0500

    update code

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Loader = () => {
   return (
-    <div className="loader-overlay">
-    <div className="spinner"></div>
-  </div>
-  )
-}
+   <div className="loader-main">
+     <div class="lds-spinner" bis_skin_checked="1">
+      <div bis_skin_checked="1"></div>
+      <div bis_skin_checked="1"></div>
+      <div bis_skin_checked="1"></div>
+      <div bis_skin_checked="1"></div>
+      <div bis_skin_checked="1"></div>
+      <div bis_skin_checked="1"></div>
+      <div bis_skin_checked="1"></div>
+      <div bis_skin_checked="1"></div>
+      <div bis_skin_checked="1"></div>
+      <div bis_skin_checked="1"></div>
+      <div bis_skin_checked="1"></div>
+      <div bis_skin_checked="1"></div>
+    </div>
+   </div>
+  );
+};
 
-export default Loader
+export default Loader;
